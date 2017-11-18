61ba4e794125ddc0241f33c3b760e59ccb38cb74
Olexandr Yermilov <olexandr.yermilov@gmail.com>
2017-11-18 02:15:05 +0200

    //stylish view, added labels

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import org.graphstream.graph.Graph;
+import org.graphstream.ui.view.Viewer;
 import сontroller.GraphReader;
 
 import java.io.FileNotFoundException;
@@ -7,6 +8,7 @@ import java.util.ArrayList;
 public class Main {
     final static String filePath = "graph.txt";
     public static void main(String[] argc){
+        System.setProperty("org.graphstream.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
         GraphReader graphReader = new GraphReader(filePath);
         try {
             graphReader.readGraph();
@@ -14,7 +16,45 @@ public class Main {
             e.printStackTrace();
         }
         Graph graphicalGraph = graphReader.getGraphicalGraph();
+        graphicalGraph.addAttribute("ui.stylesheet","graph {\n" +
+                "canvas-color: white;\n" +
+                "fill-mode: gradient-radial;\n" +
+                "fill-color: #002, #004;\n" +
+                //"padding: 20px;\n" +
+                "}\n" +
+                "node {\n" +
+                "shape: circle;\n" +
+                //"size-mode: dyn-size;\n" +
+                "size: 37px;\n" +
+                "fill-mode: gradient-radial;\n" +
+                "fill-color: #FFFC, #FFF0;\n" +
+                "stroke-mode: none;\n" +
+                "text-size: 11px;\n"+
+                "text-color: #FFFFFF;\n"+
+                "}\n" +
+                "node:clicked {\n" +
+                "fill-color: #F00A, #F000;\n" +
+                "}\n" +
+                "node:selected {\n" +
+                "fill-color: #00FA, #00F0;\n" +
+                "}\n" +
+                "edge {\n" +
+                "shape: cubic-curve;\n" +
+                "size: 1px;\n" +
+                "fill-color: #FFF3;\n" +
+                "fill-mode: plain;\n" +
+                "arrow-shape: none;\n" +
+                "text-size: 14px;\n"+
+                "text-color: #FFFFFF;\n"+
+                "text-alignment: above;\n"+
+                "}\n"/* +
+                "sprite {\n" +
+                "shape: circle;\n" +
+                "fill-mode: gradient-radial;\n" +
+                "fill-color: #FFF8, #FFF0;\n" +
+                "}"*/);
         ArrayList<ArrayList<Integer[]>> adjacencyList = graphReader.getAdjacencyList();
-        graphicalGraph.display();
+        Viewer viewer = graphicalGraph.display();
+        //graphicalGraph.getNode(0).
     }
 }
--- src/main/java/сontroller/GraphReader.java
+++ src/main/java/сontroller/GraphReader.java
@@ -14,6 +14,7 @@ public class GraphReader {
     private File fileToRead;
     private Graph graph;
     private ArrayList<ArrayList<Integer[]>> adjacencyList;
+    private int flowFrom,flowTo;
     public GraphReader(String fileToReadPath){
         try{
             this.fileToRead = new File(fileToReadPath);
@@ -38,8 +39,8 @@ public class GraphReader {
         for(int i=0;i<vertices;i++){
             adjacencyList.add(new ArrayList<Integer[]>());
         }
-        this.graph = new DefaultGraph("Main Graph");
-        while(scanner.hasNext()){
+        this.graph = new DefaultGraph("Main FFGraph");
+        for(int i=0;i<edges;i++){
             from = scanner.nextInt();
             to = scanner.nextInt();
             from--;
@@ -47,20 +48,28 @@ public class GraphReader {
             weight = scanner.nextInt();
             if(graph.getNode(from)==null){
                 this.graph.addNode("N"+from);
+                this.graph.getNode("N"+from).addAttribute("ui.label","N"+from);
             }
             Integer[] edge = {to,weight};
             adjacencyList.get(from).add(edge);
             if(graph.getNode(to)==null) {
                 this.graph.addNode("N"+to);
+                this.graph.getNode("N"+to).addAttribute("ui.label","N"+to);
             }
             if(isOriented){
                 edge[0]=from;
                 adjacencyList.get(to).add(edge);
             }
-            System.out.println("from N"+from+" to N"+to);
+            System.out.println("from N"+from+" to N"+to+" weight: "+weight);
             this.graph.addEdge("from N"+from+" to N"+to,from,to,isOriented);
+            this.graph.getEdge("from N"+from+" to N"+to).addAttribute("ui.label","0/"+weight);
+            this.graph.getEdge("from N"+from+" to N"+to).addAttribute("layout.weight",weight);
 
         }
+        flowFrom = scanner.nextInt();
+        flowFrom--;
+        flowTo = scanner.nextInt();
+        flowTo--;
         //todo: handle errors while reading
     }
     public Graph getGraphicalGraph() {
